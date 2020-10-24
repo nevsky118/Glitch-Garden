9eb89a359a1057ae3ec11612223d8ffe78c1c513
kringel118 <25124427+nevsky118@users.noreply.github.com>
2020-10-25 01:06:22 +0300

    25.10.2020

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -27,9 +27,5 @@ public class LevelLoader : MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
